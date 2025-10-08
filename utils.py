792da7294e1f5d609210c9840d5bbc3fcdcb40a8
Jandw <jan.walliser@unibas.ch>
2025-10-08 10:59:29 +0200

    Add method for testing the model on one image

--- utils.py
+++ utils.py
@@ -1,7 +1,7 @@
 import torch
 import os
 
-def save_model(model, epoch, save_dir="checkpoints"):
+def save_model(model, epoch, save_dir="Checkpoints"):
     os.makedirs(save_dir, exist_ok=True)
     save_path = os.path.join(save_dir, f"srcnn_epoch_{epoch}.pth")
     torch.save(model.state_dict(), save_path)
@@ -11,3 +11,10 @@ def load_model(model, checkpoint_path):
     model.load_state_dict(torch.load(checkpoint_path))
     print(f"Model loaded from {checkpoint_path}")
     return model
+
+def test_model_single_image(model, image_tensor,device='cpu'):
+    model.eval()
+    with torch.no_grad():
+        output = model(image_tensor.unsqueeze(0).to(device))
+
+    return output.squeeze(0).to(device)
